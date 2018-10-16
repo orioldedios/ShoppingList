40b8bd97fdaf3e5306026c591a56d0c1cde73e37
Pau Fernández <pau.fernandez@upc.edu>
2018-10-16 11:26:32 +0200

    Afegim el model i referències a elements de la pantalla.

--- app/src/main/java/info/pauek/shoppinglist/ShoppingListActivity.java
+++ app/src/main/java/info/pauek/shoppinglist/ShoppingListActivity.java
@@ -2,12 +2,30 @@ package info.pauek.shoppinglist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import java.util.List;
 
 public class ShoppingListActivity extends AppCompatActivity {
+
+    // Model
+    List<String> items;
+
+    // Referències a elements de la pantalla
+    private RecyclerView items_view;
+    private ImageButton btn_add;
+    private EditText edit_box;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_list);
+
+        items_view = findViewById(R.id.items_view);
+        btn_add = findViewById(R.id.btn_add);
+        edit_box = findViewById(R.id.edit_box);
     }
 }
